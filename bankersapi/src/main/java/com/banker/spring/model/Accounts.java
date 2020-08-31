6c96742f64e4047dbaac9b87a9848455f70cab92
Seaton <seaton@github.com>
2020-08-31 22:48:02 +0530

    Adding lombok

--- bankersapi/src/main/java/com/banker/spring/model/Accounts.java
+++ bankersapi/src/main/java/com/banker/spring/model/Accounts.java
@@ -4,9 +4,17 @@ import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor		//Now @Data will not generate @RequiredArgsConstructor
+@Data					//@Getter, @Setter, @ToString, @EqualsAndHashCode and @RequiredArgsConstructor
 @Entity
-@Table(name="Accounts")
+@Table(name="ACCOUNTS")
 public class Accounts implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
 
 
 
